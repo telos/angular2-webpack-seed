79ed370fffb24fa87abcb509c74a2375448414d2
Jacob Parma <jacob.parma@omniresources.com>
2016-10-14 10:00:06 -0500

    Update selector name to follow standards

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import '../../public/css/styles.css';
 @Component({
-  selector: 'my-app',
+  selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
